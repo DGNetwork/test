a89ba521d3e95f2d40cbab3b73e92069772a6dca
Your Name <you@example.com>
2016-11-22 15:03:27 +0900

    first commit

---
+++ app/src/main/java/com/example/dg/test/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.dg.test;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.content_main);
+
+        switch (item.getItemId()) {
+            case R.id.red:
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                mainLayout.setBackgroundColor(android.graphics.Color.RED);
+                return true;
+            case R.id.green:
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                mainLayout.setBackgroundColor(android.graphics.Color.GREEN);
+                return true;
+            case R.id.yellow:
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                mainLayout.setBackgroundColor(android.graphics.Color.YELLOW);
+                return true;
+            case R.id.blue:
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                mainLayout.setBackgroundColor(android.graphics.Color.BLUE);
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        //return super.onOptionsItemSelected(item);
+    }
+}
